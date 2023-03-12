3e768348b63936cb1a38108ed79e52b1c0d2b692
UrbanikxPatryk <patrykxurbanik@gmail.com>
2023-03-12 11:39:45 +0100

    Main

---
+++ 11.03.2023/program_11.03.2023.py
@@ -0,0 +1,30 @@
+
+import os
+
+
+dane = [
+     5, 0.7,
+     10, 0.75,
+     100, 2.4,
+     200, 4.9,
+     400, 11,
+     800, 29,
+     1600, 86,
+     3200, 224,
+     6400, 497,
+     12800, 1092]
+
+x,y = [],[] 
+
+for wynik in dane:
+     if dane.index(wynik) % 2 == 0 :
+         os.system("cls")
+         x.append(wynik)     
+     else:
+         os.system("cls")
+         y.append(wynik)
+
+a= [2,5,7,9] 
+b=[4,8,18,27]
+
+ 
