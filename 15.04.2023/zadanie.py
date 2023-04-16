589d41a0ba648a5ec744ab786588eca7ee475749
UrbanikxPatryk <patrykxurbanik@gmail.com>
2023-04-16 13:14:06 +0200

    Create zadanie.py

---
+++ 15.04.2023/zadanie.py
@@ -0,0 +1,28 @@
+s = ['aa', 'a', 'c', 'bb']
+def SPR(s):
+
+
+
+    w = set(s)
+
+    if len(s)==len(w):
+        return True
+    else:
+        return False
+
+
+
+
+def SPR2(w):
+
+    wyg=set()
+    for x in  w:
+        if w.count(x)>1:
+            wyg.add(x)
+        if len(wyg)==0:
+            return False
+
+        return True, wyg
+
+print(SPR(s))
+print(SPR2(s))
